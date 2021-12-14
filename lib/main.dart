30f9b3e5cf88b85592b1ecf431dedcc6f21a6712
Nahin-CDR <dev.nahin95@gmail.com>
2021-12-14 16:45:19 +0600

    second push | demo news App has been created

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,164 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+    return  MaterialApp(
+      home: API(),
     );
+
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+Map? mapResponse;
+Map? dataResponse;
+List? listResponse;
+class API extends StatefulWidget {
+  const API({Key? key}) : super(key: key);
+  @override
+  _APIState createState() => _APIState();
+}
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class _APIState extends State<API> {
 
-  final String title;
+
+  final url = "https://newsapi.org/v2/everything?q=apple&from=2021-12-13&to=2021-12-13&sortBy=popularity&apiKey=240ada24aa594437bc4f596da435b1f7";
+
+  Future ApiCall() async {
+    http.Response response;
+    response = await http.get(Uri.parse(url));
+    if(response.statusCode == 200){
+      setState(() {
+        mapResponse = json.decode(response.body);
+        //dataResponse = mapResponse!['articles'];
+        listResponse = mapResponse!['articles'];
+      });
+
+    }else
+      {
+        print("error");
+      }
+  }
+
+
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void initState() {
+    // TODO: implement initState
+    ApiCall();
+    super.initState();
   }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("News App"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView.builder(itemBuilder: (context,index){
+        return Container(
+          child: Column(
+            children: [
+              Container(
+                margin: new EdgeInsets.all(10),
+                //decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.green),
+                child: listResponse![index]['urlToImage'] == null ? Text("Loading Image.."):
+                Image.network(listResponse![index]['urlToImage']),
+              ),
+              Container(
+                margin: new EdgeInsets.all(1),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: listResponse![index]['author'] == null ?Text("Loading data"):
+                  Text("author : "+listResponse![index]['author'].toString(),style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey
+                  ),
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: new EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(2)),
+                child: Center(
+                  child: listResponse![index]['title'] == null ?Text("Loading data"):
+                  Text(listResponse![index]['title'].toString(),style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                  ),
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: new EdgeInsets.only(top: 10,left: 10,right: 10),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(2)),
+                child: Center(
+                  child: listResponse![index]['description'] == null ?Text("Loading data"):
+                  Text(listResponse![index]['description'].toString(),style: TextStyle(
+                      fontSize: 18
+                  ),
+                  ),
+                ),
+              ),
+
+
+              // Container(
+              //   margin: new EdgeInsets.only(left: 10,right: 10),
+              //   decoration: BoxDecoration(borderRadius: BorderRadius.circular(1)),
+              //   child: Center(
+              //     child: listResponse![index]['content'] == null ?Text("Loading data"):
+              //     Text("Content : "+listResponse![index]['content'].toString(),style: TextStyle(
+              //         fontSize: 18
+              //     ),
+              //     ),
+              //   ),
+              // ),
+              //
+
+
+
+              Container(
+                height: 30,
+                margin: new EdgeInsets.only(left: 10,right: 10,top: 10),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(2)),
+                child: Center(
+                  child: listResponse![index]['publishedAt'] == null ?Text("Loading data"):
+                  Text("published At : "+listResponse![index]['publishedAt'].toString(),style: TextStyle(
+                      fontSize: 18,
+                    color: Colors.grey
+                  ),
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: new EdgeInsets.only(top: 20,bottom: 20),
+                height: 1,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(2),color: Colors.redAccent),
+              )
+            ],
+          ),
+        );
+      },
+        itemCount: listResponse == null?0:listResponse!.length,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+     );
   }
 }
